9d37bafc50aa4f3a94c019532229a5792d6de409
Ubuntu <ubuntu@ip-172-31-67-49.ec2.internal>
2025-12-01 02:48:59 +0000

    finished gpt exp

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,17 +7,17 @@ conda activate flashrag
 # cd /home/ubuntu/FlashRAG-repro-recomp/examples/quick_start
 # python simple_pipeline.py --model_path ../../models/llama3-8b-instruct/ --retriever_path ../../models/e5-base-v2/
 
-cd /home/ubuntu/FlashRAG-repro-recomp/
-python -m flashrag.retriever.index_builder \
-    --retrieval_method e5 \
-    --model_path models/e5-base-v2/ \
-    --corpus_path datasets/retrieval-corpus/wiki18_100w_1_10.jsonl \
-    --save_dir ./wiki18_100w_1_10_indexes/ \
-    --use_fp16 \
-    --max_length 512 \
-    --batch_size 8 \
-    --pooling_method mean \
-    --faiss_type Flat
+# cd /home/ubuntu/FlashRAG-repro-recomp/
+# python -m flashrag.retriever.index_builder \
+#     --retrieval_method e5 \
+#     --model_path models/e5-base-v2/ \
+#     --corpus_path datasets/retrieval-corpus/wiki18_100w_1_10.jsonl \
+#     --save_dir ./wiki18_100w_1_10_indexes/ \
+#     --use_fp16 \
+#     --max_length 512 \
+#     --batch_size 8 \
+#     --pooling_method mean \
+#     --faiss_type Flat
 
-# cd /home/ubuntu/FlashRAG-repro-recomp/examples/methods
-# python run_exp.py --method_name 'gpt' --split 'dev' --dataset_name 'hotpotqa' --gpu_id '0' > 1_10_data_gpt_hotpotqa_topk_5.log 2>&1 &
+cd /home/ubuntu/FlashRAG-repro-recomp/examples/methods
+python run_exp.py --method_name 'gpt' --split 'dev' --dataset_name 'hotpotqa' --gpu_id '0' > 1_10_data_gpt_hotpotqa_topk_5.log 2>&1 &
